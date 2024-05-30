c7f38d2b91a355949ecfabeaa7ac195ea40d8318
lillianassefa <lillianalehegn123@gmail.com>
2024-05-30 22:36:30 +0300

    test

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import json
 import re
 
 def correct_json_like_content(content):
+    print("entered the function 1 with:" ,content)
     # Correcting missing quotes around keys
     content = re.sub(r'(\w+)\s*:', r'"\1":', content)
     # Correcting missing quotes around string values
@@ -18,8 +19,10 @@ def parse_json(content):
         exit(1)
 
 def main():
+    print("entered main")
     with open('pr_body', 'r') as file:
         content = file.read()
+    print("here is the content", content)
     corrected_content = correct_json_like_content(content)
     print("here is corrected context", corrected_content)
     data = parse_json(corrected_content)
