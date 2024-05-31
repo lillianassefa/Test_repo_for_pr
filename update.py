5351129c7bc4948cdf70f259f1dc0d3c7624f81f
lillianassefa <lillianalehegn123@gmail.com>
2024-05-31 11:34:13 +0300

    today

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import json
 import re
 
-file_path = "updates.json" 
+
 def parse_json(content):
     try:
         return json.loads(content)
@@ -11,36 +11,13 @@ def parse_json(content):
 
 def main():
     print("entered main")
-    with open('updates.json', 'r') as file:
+    with open('pr_body', 'r') as file:
         content = file.read()
     print("here is the content", content)
    
-
+    
     data = parse_json(content)
     print("Processed Data:", data)
-        # Extracting and storing data into variables
-    date = data['Date']
-    category = data['Category']
-    title = data['Title']
-    preview = data['Preview']
-    business_values = data['BusinessValues']
-    highlights = data['Highlights']
-    updates = data['Updates']
-    deprecations = data['Deprecations']
-    bug_fixes = data['BugFixes']
-    known_issues = data['KnownIssues']
-
-    # Example usage: print the extracted data
-    print(f"Date: {date}")
-    print(f"Category: {category}")
-    print(f"Title: {title}")
-    print(f"Preview: {preview}")
-    print("Business Values:", business_values)
-    print("Highlights:", highlights)
-    print("Updates:", updates)
-    print("Deprecations:", deprecations)
-    print("Bug Fixes:", bug_fixes)
-    print("Known Issues:", known_issues)
 
 if __name__ == "__main__":
     main()
